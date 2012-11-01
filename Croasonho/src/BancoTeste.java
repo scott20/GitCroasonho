8353a57da7aa1cf57049c2bd547840845c559374
630810181 <mr.scott_ganz@hotmail.com>
2012-11-01 11:13:08 -0200

    just update

--- Croasonho/src/BancoTeste.java
+++ Croasonho/src/BancoTeste.java
@@ -1,39 +1,19 @@
 import java.sql.*;
 public class BancoTeste { 
   
-    public static void main(String args[]) {
-        System.out.println("Testando acesso a banco de dado MySQL\n\n");
-        Connection conn = null;
-        String teste = "SELECT codigo,nome FROM Loja testeJava;";
-  
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection( "jdbc:mysql://localhost/test?user=root" );
-            System.out.println("A conexão foi um sucesso\n");  
-            Statement stm = conn.createStatement();  
-            ResultSet rs = stm.executeQuery(teste);
-            while (rs.next()) {
-                String nome = rs.getString("nome");
-                int codigo = rs.getInt("codigo");
-//              String sobrenome = rs.getString("sobrenome");
-                System.out.println("Codigo: " + codigo + "\nNome: " +nome ); // + "Sobrenome: " + sobrenome);
-                System.out.println("---------------------------------------");
-            }
-            System.out.println("\n\nConsulta realizada com sucesso!!!\n"); 
-        } catch(ClassNotFoundException e) {
-            System.out.println("excessão Classe não encontrada");
-            e.printStackTrace();
-        } catch(SQLException e) {
-            System.out.println("SQL Exception... Erro na consulta:");
-            e.printStackTrace();
-        } finally {
-            try {
-                conn.close();
-                System.out.println("\n\nFechamendo a conexão");
-            } catch(SQLException erro) {
-                System.out.println("Erro no fechamento");
-                erro.printStackTrace();
-            }
-        }
-    }
-} 
+    public static void main(String args[]){
+    	Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1", "user", "password");
+
+
+    			Statement stmt = con.createStatement();
+    			ResultSet rs = stmt.executeQuery("SELECT PK.Contato FROM contato");
+
+    			System.out.printf("Contas\n=====\n");
+/*
+    			while (rs.next()) {
+    			int x = rs.getInt("id");
+    			System.out.printf("%d\n", x);
+*/
+    			}
+    		}
+//} 
