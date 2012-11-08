4bca0dfe4541632ff9a49ee4a8d2edff45ffda1c
630810181 <mr.scott_ganz@hotmail.com>
2012-11-08 10:33:48 -0200

    Conecsão estabelecida

---
+++ Croasonho/src/Conect.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+
+public class Conect
+{
+    public static void main (String[] args)
+    {
+        Connection conn = null;
+
+        try
+        {
+            String userName = "root";
+            String password = "123456";
+            String url = "jdbc:mysql://127.0.0.1/croasonho";
+            Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+            conn = DriverManager.getConnection (url, userName, password);
+            System.out.println ("Conexão com o BD estabelecida!");
+        }
+        catch (Exception e)
+        {
+            System.err.println ("Não foi possível estabelecer conexão com o BD");
+        }
+        finally
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    conn.close ();
+                    System.out.println ("Conexão finalizada");
+                }
+                catch (Exception e) { /* ignore close errors */ }
+            }
+        }
+    }
+}
